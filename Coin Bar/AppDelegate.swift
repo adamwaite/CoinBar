2cfad722c533d8cdafa0b807b1b480f208dade3b
Adam Waite <adam@adamjwaite.co.uk>
2017-09-16 07:15:16 +0100

    Initial Commit

---
+++ Coin Bar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Coin Bar
+//
+//  Created by Adam Waite on 16/09/2017.
+//  Copyright © 2017 adamjwaite.co.uk. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Coin Bar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Coin Bar
+//
+//  Created by Adam Waite on 16/09/2017.
+//  Copyright © 2017 adamjwaite.co.uk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
